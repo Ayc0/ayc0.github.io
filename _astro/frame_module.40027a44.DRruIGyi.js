2b36f4b090b833cb512ef1a5fc11c7c0ea010373
Ayc0 <ayc0.benj@gmail.com>
2024-11-09 18:45:28 +0000

    Deploying to gh-pages from @ Ayc0/ayc0.github.io@34f546f9ffa5eeae7d5137b6412ecd2c3cac090b 🚀

---
+++ _astro/frame_module.40027a44.DRruIGyi.js
@@ -0,0 +1,1 @@
+const o="_frame_17ag9_1",_="_window_17ag9_13",a="_console_17ag9_17",s="_log_17ag9_33",c={frame:o,window:_,console:a,log:s,"log-separator":"_log-separator_17ag9_37","clear-console":"_clear-console_17ag9_43"};export{c};
