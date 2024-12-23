63a591960939498a772bb9fd23a1be84c4a10548
Ayc0 <ayc0.benj@gmail.com>
2024-12-23 19:32:10 +0100

    Add pubDate to RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,6 +4,21 @@ import rss from "@astrojs/rss";
 
 import { getPublishablePosts } from "@components/get-posts";
 
+import { type CollectionEntry } from "astro:content";
+
+// TODO: use the `createdAt` instead of `lastUpdated`
+const getPubDate = (post: CollectionEntry<"docs">): Date | undefined => {
+  if (post.data.lastUpdated instanceof Date) {
+    return post.data.lastUpdated;
+  }
+
+  if (typeof post.data.lastUpdated === "number") {
+    return new Date(post.data.lastUpdated);
+  }
+
+  return undefined;
+};
+
 export const GET: APIRoute = async (context) => {
   const posts = await getPublishablePosts();
 
@@ -15,8 +30,7 @@ export const GET: APIRoute = async (context) => {
     items: posts.map((post) => ({
       title: post.data.title,
       link: post.slug,
-      // TODO: add the pubDate
-      // pubDate: post.data.pubDate,
+      pubDate: getPubDate(post),
       // TODO: add description to all posts (and make it required + add it to preview to LinkToPost)
       // description: post.data.description,
     })),
