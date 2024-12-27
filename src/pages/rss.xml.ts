eb9babe665a132d3213e21902b466cab079ed5b8
Ayc0 <ayc0.benj@gmail.com>
2024-12-27 17:54:48 +0100

    Avoid double //

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,14 +4,14 @@ import rss from "@astrojs/rss";
 
 import { getPublishablePosts } from "@components/get-posts";
 
-export const GET: APIRoute = async (context) => {
+export const GET: APIRoute = async (_context) => {
   const posts = await getPublishablePosts();
 
   return rss({
     title: "Projects by Ayc0",
     description:
       "Various personal projects (finished or not), and all the blog posts I wrote",
-    site: context.site!,
+    site: "https://ayc0.github.io/",
     items: posts.map((post) => ({
       title: post.data.title,
       link: post.slug,
@@ -19,7 +19,7 @@ export const GET: APIRoute = async (context) => {
       description: post.data.description,
       enclosure: post.data.image
         ? {
-            url: context.site! + post.data.image,
+            url: `https://ayc0.github.io${post.data.image}`,
             length: 0, // no need to pass a real one, but potentially it could be better? nerdy.dev uses 0 too
             type: `image/${post.data.image.split(".").at(-1)}`,
           }
