c7a87d3cdabcd540c6a4cd408c28f95d5210fefa
Ayc0 <ayc0.benj@gmail.com>
2024-12-28 01:17:33 +0100

    Allow to use the regular resolution in RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,8 +3,9 @@ import type { APIRoute } from "astro";
 import rss from "@astrojs/rss";
 
 import { getPublishablePosts } from "@components/get-posts";
+import { getImagePath } from "@components/images";
 
-export const GET: APIRoute = async (_context) => {
+export const GET: APIRoute = async (context) => {
   const posts = await getPublishablePosts();
 
   return rss({
@@ -12,18 +13,26 @@ export const GET: APIRoute = async (_context) => {
     description:
       "Various personal projects (finished or not), and all the blog posts I wrote",
     site: "https://ayc0.github.io/",
-    items: posts.map((post) => ({
-      title: post.data.title,
-      link: post.slug,
-      pubDate: post.data.createdAt,
-      description: post.data.description,
-      enclosure: post.data.image
-        ? {
-            url: `https://ayc0.github.io${post.data.image}`,
-            length: 0, // no need to pass a real one, but potentially it could be better? nerdy.dev uses 0 too
-            type: `image/${post.data.image.split(".").at(-1)}`,
-          }
-        : undefined,
-    })),
+    items: await Promise.all(
+      posts.map(async (post) => {
+        let image: string | undefined;
+        if (post.data.image) {
+          image = await getImagePath(post.data.image);
+        }
+        return {
+          title: post.data.title,
+          link: post.slug,
+          pubDate: post.data.createdAt,
+          description: post.data.description,
+          enclosure: image
+            ? {
+                url: new URL(image, context.url).href,
+                length: 0, // no need to pass a real one, but potentially it could be better? nerdy.dev uses 0 too
+                type: `image/${image.split(".").at(-1)}`,
+              }
+            : undefined,
+        };
+      }),
+    ),
   });
 };
