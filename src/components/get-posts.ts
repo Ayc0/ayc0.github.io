924a56458c56ab83cbbb4c383e766550a22c4c36
Ayc0 <ayc0.benj@gmail.com>
2025-01-02 09:22:56 +0100

    add validation to avoid removing the slug

--- src/components/get-posts.ts
+++ src/components/get-posts.ts
@@ -14,6 +14,12 @@ export const getPublishablePosts = async () => {
     (post) => post.data.createdAt && !post.data.draft && post.data.pagefind,
   );
 
+  posts.forEach((post) => {
+    if (post.slug.startsWith("posts/drafts/")) {
+      throw new Error(`Public post ${post.id} cannot be in the drafts folder`);
+    }
+  });
+
   return posts.sort((postA, postB) => getTime(postB) - getTime(postA));
 };
 
