b4f10821dafeddb087aa427559990ab0cdf23686
Ayc0 <ayc0.benj@gmail.com>
2025-02-28 11:10:46 +0100

    Fix devtools metadata

--- src/components/experiments/page-react-extension/metadata.js
+++ src/components/experiments/page-react-extension/metadata.js
@@ -7,17 +7,19 @@ if (window.ReactDOM) {
 }
 
 if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-  console.log(
-    window.customDevTools ? "From custom DevTools" : "From DevTools",
-    [...window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.values()].map(
-      (renderer) => ({
-        reactVersion: renderer.reconcilerVersion,
-        rendererPackageName: renderer.rendererPackageName,
-        rendererVersion: renderer.version,
-      }),
-    ),
-  );
-  if (window.customDevTools) {
-    delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-  }
+  setTimeout(() => {
+    console.log(
+      window.customDevTools ? "From custom DevTools" : "From DevTools",
+      [...window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.values()].map(
+        (renderer) => ({
+          reactVersion: renderer.reconcilerVersion,
+          rendererPackageName: renderer.rendererPackageName,
+          rendererVersion: renderer.version,
+        }),
+      ),
+    );
+    if (window.customDevTools) {
+      delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+    }
+  }, 100); // The renderers aren't injected in a blocking way but asynchronously
 }
