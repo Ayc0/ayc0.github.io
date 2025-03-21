45d2fb7a575a640fa2639230dbc1a35cee273e50
Ayc0 <ayc0.benj@gmail.com>
2025-03-21 14:55:45 +0100

    Add more hydration

---
+++ src/components/experiments/page-react-extension/hydration/manual.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { hydrateRoot } from "react-dom/client";
+import * as Diff from "diff";
+
+const wrapper = document.getElementById("root")!;
+
+const before = wrapper.innerHTML;
+hydrateRoot(wrapper, <div>World</div>, {
+  onRecoverableError: (error) => {
+    console.warn("[CUSTOM] Recoverable error", error);
+    const after = wrapper.innerHTML;
+
+    console.log({ diff: Diff.diffChars(before, after) });
+  },
+});
---
+++ src/components/experiments/page-react-extension/hydration/nothing.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { hydrateRoot } from "react-dom/client";
+
+hydrateRoot(document.getElementById("root")!, <div>World</div>);
