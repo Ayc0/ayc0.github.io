aa52c2a9f9efa8c4f3c405c67d3312c6e553a65e
Ayc0 <ayc0.benj@gmail.com>
2025-03-28 21:17:20 +0000

    Deploying to gh-pages from @ Ayc0/ayc0.github.io@c9c1086753a138a79b79c793d44390e8765d6208 🚀

--- _astro/custom-pragma.astro_astro_type_script_index_0_lang.BiwrpN6M.js
+++
@@ -1,3 +0,0 @@
-import{g as s,a as c}from"./fiber.D_L7F8YT.js";import{f as a}from"./_utils.CZVZ1_aI.js";import"./chunk-ELVWOSDS.DplWDq_q.js";import"./index.F28aNuxU.js";const l=()=>{document.addEventListener("click",n=>{const i=n.target,o=s(i);if(!o)return;const r=[];let e=o;for(;e;){console.log("current.pendingProps",e.pendingProps),e.pendingProps||console.log(e);const t=window._DEBUG_MAPPED_PROPS.get(e.pendingProps);if(t?r.push(a(t)):console.warn(`[CUSTOM] No stack for "${e.elementType?.name}"`,e),e.return===e)break;e=e.return}console.log("[CUSTOM] Clicked on "+[c(o),`
-`,...r].join(""))})};l();
-//# sourceMappingURL=custom-pragma.astro_astro_type_script_index_0_lang.BiwrpN6M.js.map
