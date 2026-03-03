3839afa52cdb8a81a51172b414c96817b57bf123
Benjamin <ayc0.benj@gmail.com>
2026-03-03 08:41:38 +0100

    Ayc0/robots.txt (#185)
    * rename sitemap
    
    * add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -75,6 +75,7 @@ export default defineConfig({
 
     // Starlight comes with its own sitemap, but it makes all private pages public (and in general all pages public)
     sitemap({
+      filenameBase: "ayc0-sitemap",
       lastmod: new Date("2025-08-21"), // Update every time we want to website to be re-crawled
       filter: (page) => {
         // No slides should be exposed
