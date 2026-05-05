97e6404e7baf8aa6a28e695a42bee556312e9998
Ayc0 <ayc0.benj@gmail.com>
2026-05-05 08:47:52 +0200

    prettier

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { defineCollection } from "astro:content";
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
-import { z } from 'astro/zod'
+import { z } from "astro/zod";
 
 // TODO: add dark-mode
 const tags = [
