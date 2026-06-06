5b4938b0efb91f76466ea87683a896983b773f60
Ayc0 <ayc0.benj@gmail.com>
2026-06-06 18:45:55 +0200

    flip order in series

--- src/components/get-posts.ts
+++ src/components/get-posts.ts
@@ -172,18 +172,18 @@ export const getPublishablePostsMatchingSeries = async (
       return 0;
     }
     if (!postA.data.createdAt) {
-      return -1;
-    }
-    if (!postB.data.createdAt) {
       return 1;
     }
+    if (!postB.data.createdAt) {
+      return -1;
+    }
     if (postA.data.createdAt.getTime() !== postB.data.createdAt.getTime()) {
-      return postA.data.createdAt.getTime() - postB.data.createdAt.getTime();
+      return postB.data.createdAt.getTime() - postA.data.createdAt.getTime();
     }
 
     const postASeriesData = getSeriesData(postA)!;
     const postBSeriesData = getSeriesData(postB)!;
-    return postASeriesData.order - postBSeriesData.order;
+    return postBSeriesData.order - postASeriesData.order;
   });
 
   return postsInSameSeries;
