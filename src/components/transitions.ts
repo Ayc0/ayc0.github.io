8a201a54bbc31887d49257decd6acfb912baf4df
Ayc0 <ayc0.benj@gmail.com>
2026-06-21 13:55:22 +0200

    only inject when LinkToPost is visible

--- src/components/transitions.ts
+++ src/components/transitions.ts
@@ -1,8 +1,3 @@
-const getTransitionName = (name: string) => {
+export const getTransitionName = (name: string) => {
   return name.replace(/[^a-zA-Z0-9]+/g, "_");
-};
-
-export const getTransitionStyle = (name: string) => {
-  const transitionName = getTransitionName(name);
-  return { style: { viewTransitionName: transitionName } };
 };
